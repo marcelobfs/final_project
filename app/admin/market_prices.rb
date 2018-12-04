09ef86f03f28a25da1359097b9513a3d74ab0872
Raghu Betina <raghu@firstdraft.com>
2018-12-04 18:44:18 +0000

    Whitelist columns in MarketPrice for ActiveAdmin

--- app/admin/market_prices.rb
+++ app/admin/market_prices.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register MarketPrice do
+
+ permit_params :observation, :gender, :current_price, :thirty_days_price, :age_group
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
