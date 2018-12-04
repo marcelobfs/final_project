168521cc19ac2638f1090141f2f86ee10a7e585c
Raghu Betina <raghu@firstdraft.com>
2018-12-04 18:43:52 +0000

    Whitelist columns in Pregnancy for ActiveAdmin

--- app/admin/pregnancies.rb
+++ app/admin/pregnancies.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Pregnancy do
+
+ permit_params :earring_id, :bull_name, :born, :cattle_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
