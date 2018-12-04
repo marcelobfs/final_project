0e3f19a1db9c4d657e5dc851bbaa5b52ba94e9bf
Raghu Betina <raghu@firstdraft.com>
2018-12-04 18:42:57 +0000

    Direct association between cattle_catalogs and weightings

--- app/models/cattle_catalog.rb
+++ app/models/cattle_catalog.rb
@@ -1,6 +1,10 @@
 class CattleCatalog < ApplicationRecord
   # Direct associations
 
+  has_many   :weightings,
+             :foreign_key => "cattle_id",
+             :dependent => :destroy
+
   has_many   :pregnancies,
              :foreign_key => "cattle_id",
              :dependent => :destroy
