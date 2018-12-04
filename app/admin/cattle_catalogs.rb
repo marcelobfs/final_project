8e6392112b72347368d299157dedeb48f4a66cd0
Raghu Betina <raghu@firstdraft.com>
2018-12-04 18:43:26 +0000

    Whitelist columns in CattleCatalog for ActiveAdmin

--- app/admin/cattle_catalogs.rb
+++ app/admin/cattle_catalogs.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register CattleCatalog do
+
+ permit_params :first_weight, :source, :race, :gender, :price_per_kilo, :father, :earring_id, :purchase_or_weaning_date, :mother_earring_id, :birthdate, :status
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
